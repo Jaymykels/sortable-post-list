542622117cac889f791702a26052dc14dde7992a
Jaymykels <jaymykels69@gmail.com>
2019-11-20 17:46:34 +0100

    installed purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss")
 module.exports = {
 	"plugins": [
 		require('tailwindcss')('tailwind.config.js'),
 		require('autoprefixer')(),
+		//Only add purgecss in production
+		process.env.NODE_ENV === "production" ? purgecss({
+			content: [
+				"./src/**/*.html",
+				"./src/**/*.vue"
+			]
+		}) : ""
 	]
 }
