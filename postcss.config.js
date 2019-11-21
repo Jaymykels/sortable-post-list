11fa1be4d7c3dc19462c206fc4a8e37b03e037b5
Jaymykels <jaymykels69@gmail.com>
2019-11-21 09:22:58 +0100

    fixed purgecss ommiting some classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
-const purgecss = require("@fullhuman/postcss-purgecss")
 module.exports = {
 	"plugins": [
 		require('tailwindcss')('tailwind.config.js'),
 		require('autoprefixer')(),
 		//Only add purgecss in production
-		process.env.NODE_ENV === "production" ? purgecss({
-			content: [
-				"./src/**/*.html",
-				"./src/**/*.vue"
-			]
+		process.env.NODE_ENV === "production" ? require('@fullhuman/postcss-purgecss')({
+			content: ['./public/**/*.html', './src/**/*.vue'],
+			defaultExtractor: (content) => {
+				const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+				return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+			},
+			whitelistPatterns: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/],
 		}) : ""
 	]
 }
